e359a3bccb76c52610bd821d5d3758a3abb2b5a0
BeckyLashua <beckylashua@gmail.com>
2021-02-23 13:51:40 -0500

    Completed rat carousel functionality

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const express = require('express');
 // Creates our express server
 const app = express();
 
-const port = 3000;
 
 let exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
+const port = 3000;
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
@@ -22,9 +23,23 @@ app.get('/', (req, res) => {
   const fs = require('fs');
   fs.readFile("views/data/ratties.json", function(err, data) {
     if (err) throw err;
-    const ratties = JSON.parse(data);
-    res.render('home', ratties);
+    const parsedData = JSON.parse(data);
+    console.log(parsedData);
+    res.render('home', parsedData);
   });
+});
+
+app.use(function(req,res) {
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
+});
+  
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.send('500 - Server Error'); 
 });
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
